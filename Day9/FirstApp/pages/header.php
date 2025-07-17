a2522332383fd98c79341b43057432aa4dfa0394
Mariam El-Habashi <mariamhabshi177@gmail.com>
2025-07-17 03:53:45 +0300

    final stage

---
+++ Day9/FirstApp/pages/header.php
@@ -0,0 +1,25 @@
+<?php 
+echo __DIR__;
+include("../coasdasnnecting.php");
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+<link rel="stylesheet" href=<?php echo ROOT_STYLE  ?>>
+</head>
+
+<body>
+
+<nav>
+    <ul>
+        <li><a href="Home.php">Home</a></li>
+        <li><a href="profile.php">Profile</a></li>
+        <li><a href="signup.php">Sign Up</a></li>
+        <li><a href="login.php">Login</a></li>
+    </ul>
+</nav>
+<div class="clearfix"></div>
---
+++ Day9/FirstApp/part/header.php
@@ -0,0 +1,38 @@
+<?php 
+include(__DIR__ . "\..\connecting.php");
+if(!@session_start()){
+    @session_start();
+}
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+<link rel="stylesheet" href=<?php echo ROOT . "\css\style.css"  ?>>
+</head>
+
+<body>
+
+<nav>
+    <ul>
+
+        <li><a href="Home.php">Home</a></li>
+        <li><a href="profile.php">Profile</a></li>
+                <?php 
+        
+        if(isset($_SESSION['xyz']) ){
+            @session_start();
+            echo "<li><a href=logout.php>Logout</a></li>";
+            echo "<li> Welcome " .$_SESSION['xyz']['username'].  "</li>";
+        } else{
+
+        ?>
+        <li><a href="signup.php">Sign Up</a></li>
+        <li><a href="login.php">Login</a></li>
+        <?php }?>
+    </ul>
+</nav>
+<div class="clearfix"></div>
